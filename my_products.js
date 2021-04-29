0c13943ca8db5f05d4995b74fc7cd06d3f25fc5d
garsan84 <jgrsan84@gmail.com>
2021-04-29 12:10:58 +0200

    cart

---
+++ my_products.js
@@ -0,0 +1,20 @@
+var products = {
+  123: {
+    name: "HP Thicc",
+    desc: "Greatest properly off ham exercise all.",
+    img: "hp.jpg",
+    price: 2034,
+  },
+  124: {
+    name: "LENOVO Rookie",
+    desc: "Unsatiable its possession nor off.",
+    img: "lenovo.jpg",
+    price: 1500,
+  },
+  125: {
+    name: "iPong Max",
+    desc: "All difficulty unreserved the solicitude.",
+    img: "samsung.jpg",
+    price: 675,
+  },
+};
